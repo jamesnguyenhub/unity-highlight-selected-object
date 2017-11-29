fc4e69bc26259f37dc2539382da1007dd09f1458
Tuyen Nguyen <nntuyen92@gmail.com>
2017-11-30 06:10:41 +0700

    Initial commit

---
+++ Assets/Scripts/HighlightController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class HighlightController : MonoBehaviour {
+  private HighlightObject highlightObject;
+
+  public void SelectObject(HighlightObject highlightObject) {
+    if (this.highlightObject != null) {
+      this.highlightObject.StopHighlight();
+    }
+
+    this.highlightObject = highlightObject;
+    this.highlightObject.StartHighlight();
+  }
+}
